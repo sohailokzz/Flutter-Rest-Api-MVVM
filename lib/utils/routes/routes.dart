d1b92be383514d588442ecee6ce57e7007e53462
sohailokzz@gmail.com <sohailokzz@gmail.com>
2023-02-05 01:02:58 +0500

    Routes Added

---
+++ lib/utils/routes/routes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:rest_api_mvvp/view/home_screen.dart';
+import 'package:rest_api_mvvp/view/login_screen.dart';
+import 'route_names.dart';
+
+class Routes {
+  static MaterialPageRoute generateRoutes(RouteSettings settings) {
+    switch (settings.name) {
+      case RoutesName.home:
+        return MaterialPageRoute(
+          builder: (BuildContext context) => const HomeScreen(),
+        );
+
+      case RoutesName.login:
+        return MaterialPageRoute(
+          builder: (BuildContext context) => const LoginScreen(),
+        );
+
+      default:
+        return MaterialPageRoute(
+          builder: (_) {
+            return const Scaffold(
+              body: Center(
+                child: Text('No Route Define'),
+              ),
+            );
+          },
+        );
+    }
+  }
+}
