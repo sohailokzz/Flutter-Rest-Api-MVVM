f16846c3004013a139b3cf873b3cdb024ccc1b8f
sohailokzz@gmail.com <sohailokzz@gmail.com>
2023-02-04 23:44:28 +0500

    Base and Network Api Added

---
+++ lib/data/network/network_api_services.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:rest_api_mvvp/data/app_exception.dart';
+import 'package:rest_api_mvvp/data/network/base_api_services.dart';
+
+class NetworkApiResponse extends BaseApiServices {
+  @override
+  Future getGetApiResponse(String url) async {
+    dynamic responseJson;
+    try {
+      final response = await http.get(Uri.parse(url)).timeout(
+            const Duration(seconds: 10),
+          );
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Connection');
+    }
+
+    return responseJson;
+  }
+
+  @override
+  Future getPostApiResponse(String url, dynamic data) async {
+    dynamic responseJson;
+    try {
+      Response response = await post(
+        Uri.parse(url),
+        body: data,
+      ).timeout(const Duration(seconds: 10));
+
+      responseJson = returnResponse(response);
+    } on SocketException {
+      throw FetchDataException('No Internet Exception');
+    }
+    return responseJson;
+  }
+
+  returnResponse(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        dynamic responseJson = jsonDecode(response.body);
+        return responseJson;
+      case 400:
+        throw BadRequestException(response.body.toString());
+      case 404:
+        throw UnAuthorizeException(response.body.toString());
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error Accured while communicating with serverwith status code ${response.statusCode}');
+    }
+  }
+}
